31efcd61c8c7d0011410996b77f1ea6c87b09a56
James Burge <james3906@gmail.com>
2025-07-24 22:09:44 +0100

    setlistfm.ts api setup

--- src/app/add-gig/page.tsx
+++ src/app/add-gig/page.tsx
@@ -1,7 +1,10 @@
-export default function AddGig() {
+import { fetchSetlistsByArtist } from "@/app/api/setlistfm";
+
+export default async function AddGig() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-2xl font-bold">Create a New Gig</h1>
+      <h1 className="text-2xl font-bold">Add a New Gig</h1>
+      <Card></Card>
     </div>
   );
 }
---
+++ src/app/api/setlistfm.ts
@@ -0,0 +1,24 @@
+const API_BASE_URL = "https://api.setlist.fm/rest/1.0";
+const API_KEY = process.env.SETLIST_FM_API_KEY!;
+const USER_AGENT = process.env.USER_AGENT!;
+
+export async function fetchSetlistsByArtist(artistName: string) {
+  const url = `${API_BASE_URL}/search/setlists?artistName=${encodeURIComponent(
+    artistName
+  )}`;
+
+  const res = await fetch(url, {
+    headers: {
+      Accept: "application/json",
+      "x-api-key": API_KEY,
+      "User-Agent": USER_AGENT,
+    },
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error(`Failed to fetch setlists: ${res.statusText}`);
+  }
+
+  return res.json();
+}
